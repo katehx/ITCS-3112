d471fa6e3a65d99e9e5de59511f7a05b395c8e58
Kate Hicks <Katehicks329@gmail.com>
2024-08-29 19:30:01 -0400

    Set up

---
+++ semester.java
@@ -0,0 +1,18 @@
+public class Semester{
+
+    public void Semester(){
+
+    }
+
+    public void Semester(int year, String season, String[] courses){
+        year = this.year;
+        season = this.season;
+        courses = this.courses
+    }
+
+
+
+
+
+
+}//end class
